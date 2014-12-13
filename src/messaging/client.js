ce65c599dfd505179a78bffdcaf3d6246e13fa2a
Andrey Listochkin <listochkin@outlook.com>
2014-12-13 15:20:04 +0000

    leveldb messaging

--- src/messaging/client.js
+++ src/messaging/client.js
@@ -1,17 +1,20 @@
-console.log('HELLO');
-
-var engine = require("engine.io-stream")
+var engine = require("engine.io-stream"),
+    multilevel = require('multilevel'),
+    liveStream = require('level-live-stream'),
+    container = require('container-el');
 
 // attach to an engine.io server at url '/numbers'
 var stream = engine("/messages")
 
-stream.on('data', function(data) {
-  console.log(data)
-  stream.write('ack')
+var manifest = require('./manifest.json');
+var db = multilevel.client(manifest);
+
+stream.pipe(db.createRpcStream()).pipe(stream);
+
+db.createLiveStream().on('data', function (record) {
+  var p = document.createElement('p');
+  p.innerHTML = record.value.text;
+  container.appendChild(p);
 });
 
-var n = 0;
-setInterval(function () {
-  stream.write(n++);
-}, 100)
-
+window.db = db;
--- src/messaging/server.js
+++ src/messaging/server.js
@@ -4,7 +4,24 @@
 var http = require('http'),
     browserify = require('browserify'),
     path = require('path'),
-    es = require("engine.io-stream");
+    es = require("engine.io-stream"),
+    levelup = require('levelup'),
+    multilevel = require('multilevel'),
+    liveStream = require('level-live-stream');
+
+var db = levelup('/path/to/file', {
+  valueEncoding: 'json',
+  db: require('memdown')
+});
+
+db.put('key', 'hahaha!', function (error) {
+  db.get('key', function (error, value) {
+    console.log(value);
+  });
+});
+
+liveStream.install(db);
+multilevel.writeManifest(db, __dirname + '/manifest.json');
 
 var server = http.createServer(function (req, res) {
   if (req.url == '/') {
@@ -15,14 +32,8 @@ var server = http.createServer(function (req, res) {
 });
 
 var engine = es(function (stream) {
-  var n = 0;
-  setInterval(function () {
-    stream.write(n++);
-  }, 50);
-
-  stream.on('data', function (data) {
-    console.log(data);
-  });
+  var magicStream = multilevel.server(db);
+  stream.pipe(magicStream).pipe(stream);
 });
 
 engine.attach(server, "/messages");
