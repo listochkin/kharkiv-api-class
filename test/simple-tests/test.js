fae7fa0d308a170d8b214935c7ad79eb566cac61
Andrey Listochkin <listochkin@outlook.com>
2014-12-13 10:40:34 +0000

    promises

--- test/simple-tests/test.js
+++ test/simple-tests/test.js
@@ -6,6 +6,7 @@ var chai = require('chai'),
     expect = chai.expect;
 
 var request = require('request');
+var jsdom = require("jsdom");
 
 describe('Simple Node tests', function () {
 
@@ -18,6 +19,46 @@ describe('Simple Node tests', function () {
     request('https://www.npmjs.org/', function (error, response, body) {
       assert(error == null, 'Error connecting to host');
       expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
+
+describe('Http tests', function () {
+
+// <div id="home-stats">
+//       <div class="stat icon-package-hex">
+//         <strong class="pretty-number">112023</strong>
+//         total packages
+//       </div>
+
+  it('Should load # of packages', function (done) {
+    request('http://nodejs.org/dist/', function (error, response, body) {
+      assert(error == null, 'Error connecting to host');
+      expect(response.statusCode).to.equal(200);
+      jsdom.env(response.body, function (error, window) {
+        var releaseCount = window.document.querySelectorAll("a").length;
+        expect(releaseCount).to.be.greaterThan(200);
+        done();
+      });
+    });
+  });
+
+  var promisify = require('bluebird').promisify,
+      http = promisify(function (options, cb) {
+        request(options, function (error, response, body) {
+          cb(error, response);
+        })
+      }),
+      dom = promisify(jsdom.env);
+
+  it('Should load # of packages with Promises', function (done) {
+    http('http://nodejs.org/dist/').then(function (response) {
+      expect(response.statusCode).to.equal(200);
+      return dom(response.body);
+    }).then(function (window) {
+      var releaseCount = window.document.querySelectorAll("a").length;
+      expect(releaseCount).to.be.greaterThan(200);
       done();
     });
   });
